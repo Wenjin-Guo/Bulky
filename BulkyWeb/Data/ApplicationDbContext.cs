cdddb6def07265060d7fff32574ac8ded2e812b7
simon <simonguo.eng@yahoo.com>
2023-05-27 14:12:54 -0400

    add category table in the SQL server by adding code in applicationDbcontext.cs

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -12,5 +12,16 @@ namespace BulkyWeb.Data
 
         //create "Categories" table from category.cs - - ->then in package Manager console add-migration , update-database
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 }, 
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Horri", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Romance", DisplayOrder = 4 }
+            );
+
+        }
     }
 }

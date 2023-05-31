e8d0c204488062915272374d2304d40d10d1803b
simon <simonguo.eng@yahoo.com>
2023-05-31 08:30:54 -0400

    add edit controller in categorycontroller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -43,8 +43,43 @@ namespace BulkyWeb.Controllers
             {
                 return View();
             }
-            
-            
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categroFromDb = _db.Categories.Find(id);
+            if(categroFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categroFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)   //need to pass the object from create.cshtml
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot exactly match the name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("name", "Test is not a valid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);       //keep track of inputs needed to be add to the database
+                _db.SaveChanges();              //in database create the category obj
+                return RedirectToAction("Index");   //redirect page to category Index.cshtml
+            }
+            else
+            {
+                return View();
+            }
+
+
         }
     }
 }

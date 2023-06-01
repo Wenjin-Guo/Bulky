77b566f983c31c2dc775ace474574628b53c7d13
simon <simonguo.eng@yahoo.com>
2023-05-31 20:21:45 -0400

    add hidden input(comment) as if the name "Id" is not the primary key in the table

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -62,17 +62,9 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)   //need to pass the object from create.cshtml
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The display order cannot exactly match the name");
-            }
-            if (obj.Name != null && obj.Name.ToLower() == "test")
-            {
-                ModelState.AddModelError("name", "Test is not a valid value");
-            }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);       //keep track of inputs needed to be add to the database
+                _db.Categories.Update(obj);       //keep track of inputs needed to be add to the database
                 _db.SaveChanges();              //in database create the category obj
                 return RedirectToAction("Index");   //redirect page to category Index.cshtml
             }
@@ -80,8 +72,6 @@ namespace BulkyWeb.Controllers
             {
                 return View();
             }
-
-
         }
     }
 }

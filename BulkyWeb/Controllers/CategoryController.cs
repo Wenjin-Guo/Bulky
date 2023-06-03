f1f822224712a1365cf1ca0c92b4e259699f3a77
simon <simonguo.eng@yahoo.com>
2023-06-02 22:08:09 -0400

    configure the delete in category controller and create a delete view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -73,5 +73,32 @@ namespace BulkyWeb.Controllers
                 return View();
             }
         }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);      //another 2 methods to find the id
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)   //need to pass the object from create.cshtml
+        {
+            Category obj = _db.Categories.Find(id);
+            if(obj == null) 
+            { 
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

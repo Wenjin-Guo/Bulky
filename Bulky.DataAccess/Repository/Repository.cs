34d9c759d3f8a2e68f3aec4c7833f921269b785c
simon <simonguo.eng@yahoo.com>
2023-06-26 09:33:31 -0400

    let product category name populate in the product index page

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -19,21 +19,40 @@ namespace Bulky.DataAccess.Repository
             _db = db;
             this.dbSet=_db.Set<T>();
             //__db.categories==dbSet
+            //include product category list using foreign key relation
+            _db.Products.Include(u => u.Category).Include(u => u.CategoryId);
 
         }
         public void Add(T entity)
         {
             dbSet.Add(entity);
         }
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
-        public IEnumerable<T> GetAll()
+        //Category
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if(!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach(var includeProp in includeProperties
+                    .Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries)) 
+                    {
+                        query = query.Include(includeProp);
+                    }
+            }
             return query.ToList();
         }
         public void Remove(T entity)

b1e5da25f29cb40be034e4601dc7d80664555db9
simon <simonguo.eng@yahoo.com>
2023-08-15 08:54:05 -0400

    add applicationUser repository, add it to unitOfWork, and enable the shoppingCart in Customer detail page by configuring the HomeController

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -25,9 +25,18 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-            return View(product);
-        }
+            ShoppingCart shoppingCart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+            return View(shoppingCart);  
+        }   
+        
+        
+    
+             
 
         public IActionResult Privacy()
         {

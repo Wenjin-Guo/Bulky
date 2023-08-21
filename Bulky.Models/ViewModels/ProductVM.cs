61f7cb6d4ce6b1a6d72935300d049b4abadf2499
simon <simonguo.eng@yahoo.com>
2023-08-21 08:11:05 -0400

    add shopping cart viewmodel, shoppingcart UI

--- Bulky.Models/ViewModels/ProductVM.cs
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bulky.Models.ViewModels
+namespace Bulky.Models.ProductVM
 {
     public class ProductVM
     {
         public Product Product { get; set; }
+        [ValidateNever]
         public IEnumerable<SelectListItem> CategoryList { get; set; }
     }
 }
